5d678988eddede5dc024333ca7d50460780d6015
Timotej Avsec <avsectimotej1@gmail.com>
2022-12-28 10:26:58 +0100

    Add prometheus, initialize consul

--- main.go
+++ main.go
@@ -1,42 +1,45 @@
 package main
 
 import (
-    "github.com/gin-contrib/cors"
-    "github.com/gin-gonic/gin"
-    swaggerfiles "github.com/swaggo/files"
-    ginSwagger "github.com/swaggo/gin-swagger"
-    healthcheck "github.com/tavsec/gin-healthcheck"
-    "github.com/tavsec/gin-healthcheck/checks"
-    "microservice-pot/controllers"
-    docs "microservice-pot/docs"
-    "microservice-pot/initializers"
-    "microservice-pot/middlewares"
-    "time"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	healthcheck "github.com/tavsec/gin-healthcheck"
+	"github.com/tavsec/gin-healthcheck/checks"
+	ginprometheus "github.com/zsais/go-gin-prometheus"
+	"microservice-pot/controllers"
+	docs "microservice-pot/docs"
+	"microservice-pot/initializers"
+	"microservice-pot/middlewares"
+	"time"
 )
 
 func init() {
-    initializers.LoadEnvVariables()
+	initializers.LoadEnvVariables()
+	initializers.InitializeConsul()
 }
 
 func main() {
-    r := gin.Default()
-    docs.SwaggerInfo.BasePath = ""
+	r := gin.Default()
+	docs.SwaggerInfo.BasePath = ""
 
-    r.Use(cors.New(cors.Config{
-        AllowOrigins:     []string{"*"},
-        AllowMethods:     []string{"*"},
-        AllowHeaders:     []string{"Origin"},
-        ExposeHeaders:    []string{"Content-Length"},
-        AllowCredentials: true,
-        MaxAge:           12 * time.Hour,
-    }))
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"*"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
-    r.Use(middlewares.MaintenanceMode())
+	r.Use(middlewares.MaintenanceMode())
+	p := ginprometheus.NewPrometheus("gin")
+	p.Use(r)
+	healthcheck.New(r, healthcheck.DefaultConfig(), []checks.Check{})
 
-    healthcheck.New(r, healthcheck.DefaultConfig(), []checks.Check{})
+	r.GET("/path", controllers.RouteGet)
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 
-    r.GET("/path", controllers.RouteGet)
-    r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
-
-    r.Run()
+	r.Run()
 }
